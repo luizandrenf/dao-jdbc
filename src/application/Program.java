2002363d6f7589f425f8d92723dc57d787f78669
Luiz André <luizandrenf@gmail.com>
2024-04-19 14:48:46 -0400

    Department entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,11 @@
 package application;
 
+import model.entities.Department;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Department ob = new Department(1, "Books");
+        System.out.println(ob);
     }
 }
